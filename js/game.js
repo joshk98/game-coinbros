2b362efcf8271103eb95a7e7baeed9f0b2bce58a
Josh Kuhn <joshkkuhn@gmail.com>
2024-03-17 15:29:45 +0000

    New map, tiles, and sprites. Some changes to code and added variables.

--- js/game.js
+++ js/game.js
@@ -1,34 +1,42 @@
+let charGravity = 500;
+let charJump = -230;
+let charSpeed = 100;
+let enemyGravity = charGravity;
+let enemySpeed = 0.33 * charSpeed;
+
+// const tileSize = 16;
+// const mapWidth = 10;
+// const mapHeight = 16;
+
 class Main {
   preload() {
-    this.load.image('background', 'assets/background.png');
     this.load.image('player', 'assets/player.png');
-    this.load.image('wallV', 'assets/wallVertical.png');
-    this.load.image('wallH', 'assets/wallHorizontal.png');
+    this.load.image('tileset', 'assets/tile-grass.png');
+    this.load.tilemapTiledJSON('map', 'assets/maps/map.json');
     this.load.image('coin', 'assets/coin.png');
     this.load.image('enemy', 'assets/enemy.png');
   }
 
   create() {
-    this.player = this.physics.add.sprite(250, 170, 'player');
-    this.player.body.gravity.y = 500;
+    this.player = this.physics.add.sprite(80, 144, 'player');
+    this.player.body.gravity.y = charGravity;
 
     this.arrow = this.input.keyboard.createCursorKeys();
 
     this.createWorld();
     
-    this.coin = this.physics.add.sprite(60, 130, 'coin');
+    this.coin = this.physics.add.sprite(80, 128, 'coin');
 
     this.scoreLabel = this.add.text(30, 25, 'Score: 0', { font: '18px Arial', fill: '#fff' });
     this.score = 0
 
     this.enemies = this.physics.add.group();
     this.time.addEvent({
-      delay: 2200,
+      delay: Phaser.Math.RND.between(2500, 4000),
       callback: () => this.addEnemy(),
       loop: true,
     });
-
-  }
+}
 
   update() {
     this.physics.collide(this.player, this.walls);
@@ -36,9 +44,9 @@ class Main {
     
     this.movePlayer();
 
-    if (this.player.y > 850 || this.player.y < 0) {
+    if (!this.physics.world.bounds.contains(this.player.x, this.player.y)) {
       this.playerDie();
-    }
+  }  
 
     if (this.physics.overlap(this.player, this.coin)) {
       this.takeCoin();
@@ -52,35 +60,28 @@ class Main {
 
   movePlayer() {
     if (this.arrow.left.isDown) {
-      this.player.body.velocity.x = -200;
+      this.player.body.velocity.x = -75;
     }
     else if (this.arrow.right.isDown) {
-      this.player.body.velocity.x = 200;
+      this.player.body.velocity.x = 75;
     }
     else {
       this.player.body.velocity.x = 0;
     }
     if (this.arrow.up.isDown && this.player.body.onFloor()) {
-      this.player.body.velocity.y = -320;
+      this.player.body.velocity.y = charJump;
     }
   }
 
   createWorld() {
-    this.walls = this.physics.add.staticGroup();
+    let map = this.add.tilemap('map');
+    let tileset = map.addTilesetImage('tileset', 'tileset');
+    this.walls = map.createLayer('Tile Layer 1', tileset);
 
-    this.walls.create(10, 170, 'wallV');
-    this.walls.create(490, 170, 'wallV');
+    this.walls.setCollision([1, 2, 3, 4, 5, 6]);
 
-    this.walls.create(50, 10, 'wallH');
-    this.walls.create(450, 10, 'wallH');
-    this.walls.create(50, 330, 'wallH');
-    this.walls.create(450, 330, 'wallH');
-
-    this.walls.create(0, 170, 'wallH');
-    this.walls.create(500, 170, 'wallH');
-    this.walls.create(250, 90, 'wallH');
-    this.walls.create(250, 250, 'wallH');
-  }
+    this.physics.world.setBoundsCollision(true, true, true, true);
+}
 
   playerDie() {
     this.scene.start('main');
@@ -88,12 +89,22 @@ class Main {
 
   updateCoinPosition() {
     let positions = [
-      { x: 140, y: 60 },
-      { x: 360, y: 60 },
-      { x: 60, y: 140 },
-      { x: 440, y: 140 },
-      { x: 130, y: 300 },
-      { x: 370, y: 300 }
+      { x: 24, y: 40 },
+      { x: 40, y: 40 },
+      { x: 120, y: 40 },
+      { x: 136, y: 40 },
+
+      { x: 24, y: 136 },
+      { x: 40, y: 136 },
+      { x: 120, y: 136 },
+      { x: 136, y: 136 },
+
+      { x: 24, y: 232 },
+      { x: 40, y: 232 },
+      { x: 56, y: 232 },
+      { x: 104, y: 232 },
+      { x: 120, y: 232 },
+      { x: 136, y: 232 },
     ];
 
     positions = positions.filter(coin => coin.x !== this.coin.x);
@@ -111,10 +122,10 @@ class Main {
   }
 
   addEnemy() {
-    let enemy = this.enemies.create(250, -10, 'enemy');
+    let enemy = this.enemies.create(80, -10, 'enemy');
 
-    enemy.body.gravity.y = 500;
-    enemy.body.velocity.x = Phaser.Math.RND.pick([-100, 100]);
+    enemy.body.gravity.y = charGravity;
+    enemy.body.velocity.x = Phaser.Math.RND.pick([-25, 25]);
     enemy.body.bounce.x = 1;
 
     this.time.addEvent({
@@ -125,11 +136,16 @@ class Main {
 };
 
 let game = new Phaser.Game({
-  width: 500,
-  height: 850,
-  backgroundColor: '#3498db',
+  width: 160,
+  height: 256,
+  backgroundColor: '#25131a',
   physics: { default: 'arcade' },
-  parent:'game',
+  parent: 'game',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  pixelArt: true,
 });
 
 game.scene.add('main', Main);
